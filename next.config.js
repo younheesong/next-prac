6b38fa0b23ab4c40261def6f161b9470ac276b09
younhee52 <younhee.song@ebridge-world.com>
2022-06-27 21:04:44 +0900

    feat: rewrite & redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "https://naver.com",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path",
+        destination: "https://naver.com",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
-const API_KEY = "01fef168465088515b14e07aa8f32453";
+
 export default function Home() {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
       console.log(results);
     })();
   }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title={"home"} />
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <h4> {movie.original_title}</h4>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
         </div>
       ))}
       <style jsx>{`
